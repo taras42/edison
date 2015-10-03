5ac2d2f6d0fea7ac03905bb78716c78305d21dde
unknown <taras.my.mail@gmail.com>
2015-10-03 09:20:48 +0100

    Installed commmader package

--- edison.js
+++ edison.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('i am the edison, fear me!');
+var fs = require('fs'),
+	sequest = require("sequest"),
+	program = require("commander");
+
+program
+  .command("init")
+  .description("Initialize project configuration file")
+  .action(function(env, options){
+    console.log("Hell yeah!");
+  });
+
+program.parse(process.argv);
